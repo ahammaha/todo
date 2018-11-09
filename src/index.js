066925f81347abdb31586c75d7174334047b0a2d
ahammaha <mahalakshmi@outlook.com>
2018-11-09 10:53:59 +0530

    add todo done

--- src/index.js
+++ src/index.js
@@ -6,38 +6,91 @@ class Todo extends React.Component{
 	render(){
 		return(
 			<li key={this.props.name}>{this.props.name}</li>
-		)
+		);
+	}
+}
+
+class AddTodo extends React.Component{
+
+	constructor(props){
+		super(props);
+		this.state={newTodoVal:this.props.newTodo}
+		this.onInputChange=this.onInputChange.bind(this);
+		this.addNewTodo=this.addNewTodo.bind(this);
+	}
+
+	onInputChange(e){
+		let val=e.target.value;
+		this.setState({newTodoVal:val});
+	}
+
+	addNewTodo(e){
+		if((e.type==="keydown" && e.key==="Enter") || (e.type==="click")){
+			this.props.addNewTodo(this.state.newTodoVal);
+			this.setState({newTodoVal:""});
+		}
+	}
+
+	render(){
+		return(
+			<fieldset>
+				<legend>Add Todo Task</legend>
+				<input type="text" 
+					value={this.state.newTodoVal} 
+					onKeyDown={this.addNewTodo} 
+					onChange={this.onInputChange} />
+				<input type="button" value="Add Todo Task" onClick={this.addNewTodo} />
+			</fieldset>
+		);
 	}
 }
 
 class TodoList extends React.Component{
+	
+	render(){
+		let todos=this.props.todolist;
+		if(todos.length){
+			return(
+				<ul>
+					{todos.map((todo)=> <Todo key={todo.name} name={todo.name} />)}
+				</ul>
+			);
+		}else{
+			return(<h1>No tasks created...</h1>);
+		}
+	}
+}
 
+
+class TodoApp extends React.Component{
 	constructor(props){
 		super(props);
 		this.state={
 			todolist:[
 				{name:"task1",completed:false},
 				{name:"task2",completed:false}
-			]
+			],
+			newTodo:""
 		}
+		this.addNewTodo=this.addNewTodo.bind(this);
+	}
+
+	addNewTodo(newTodoVal){
+		let val={name:newTodoVal,completed:false}
+		this.setState({todolist:[...this.state.todolist,val]});
 	}
 
 	render(){
-		let todolist=this.state.todolist;
-		if(todolist.length){
-			return(
-				<ul>
-					{todolist.map((todo)=><Todo key={todo.name} name={todo.name} />)}
-				</ul>
-			)
-		}else{
-			return(<h1>No tasks created...</h1>)
-		}
+		return(
+			<div>
+				<AddTodo newTodo={this.state.newTodo} addNewTodo={this.addNewTodo} />
+				<TodoList todolist={this.state.todolist}  />
+			</div>
+		);
 	}
 }
 
-
 ReactDOM.render(
-	<TodoList />,
+	<TodoApp />,
 	document.getElementById("root")
 );
